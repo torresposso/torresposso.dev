3a500bb172c7710a669c9650cbcfa51c37f7e81e
erictorresco <erictorres.co@gmail.com>
2024-03-30 23:29:50 -0500

    fix imports

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,8 +1,6 @@
-import { useSignal } from "@preact/signals";
 import { Hero } from "@/islands/Hero.tsx";
 
 export default function Home() {
-  const count = useSignal(3);
   return (
     <>
       <Hero />
