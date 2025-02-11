87f89f0782db350190c3a0101661a56d2c342292
torresposso <erictorres.co@gmail.com>
2025-02-11 13:17:03 -0500

    add node mode to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,15 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://torresposso.dev',
-	integrations: [mdx(), sitemap()],
-	compressHTML: true,
+  site: 'https://torresposso.dev',
+  integrations: [mdx(), sitemap()],
+  compressHTML: true,
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
